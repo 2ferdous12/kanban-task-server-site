3d0295a1b9b8e38ee38e3e24e986f9ce68af84a7
Ferdous020 <sheikhferdous75@gmail.com>
2024-05-07 16:04:16 -0700

    added all file and style

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
 require('dotenv').config();
 
 // middle ware
 app.use(cors());
 app.use(express.json());
 
-// console.log(process.env.DB_USER)
-// console.log(process.env.DB_PASS)
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster000.74jenv3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster000`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -29,11 +24,24 @@ async function run() {
     await client.connect();
 
     const Colection = client.db('usersDB').collection('users');
+    const taskCollections = client.db('usertaskDB').collection('usertask');
 
     app.get('/users', async(req, res)=>{
         const cursor = Colection.find();
         const result = await cursor.toArray();
         res.send(result);
+        })
+    app.get('/use', async(req, res)=>{
+        const items = taskCollections.find();
+        const result = await items.toArray();
+        res.send(result);
+        })
+
+        app.delete('/users/:id', async(req, res) =>{
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id)}
+          const result = await Colection.deleteOne(query);
+          res.send(result);
         })
 
     app.post('/users', async(req, res)=>{
@@ -42,6 +50,19 @@ async function run() {
         const result = await Colection.insertOne(newboard);
         res.send(result);
       })
+    app.post('/use', async(req, res)=>{
+        const newboar = req.body;
+        console.log(newboar)
+        const result = await taskCollections.insertOne(newboar);
+        res.send(result);
+      })
+
+  //  app.get("/users/:colum1", async(req, res)=>{
+  //     const item = req.params.colum1;
+  //     const query = {colum1: new ObjectId(item)}
+  //     const result = await Colection.findOne(query)
+  //     res.send(result)
+  //   })
 
 
     await client.db("admin").command({ ping: 1 });
